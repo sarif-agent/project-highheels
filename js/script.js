25ff4528499be199401c1f3c3112586ad0e7e16e
Berkan K <berkan0hk@gmail.com>
2024-10-02 02:11:37 +0300

    all around done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+console.log(`Console Says: "I'm Alive!"`);
+
+const searchForm = document.querySelector(".search-form");
+const cartItem = document.querySelector(".cart-items-container");
+const navbar = document.querySelector(".navbar");
+
+//! buttons
+const searchBtn = document.querySelector("#search-btn");
+const cartBtn = document.querySelector("#cart-btn");
+const menuBtn = document.querySelector("#menu-btn");
+
+document.addEventListener("dragstart", function (event) {
+  if (event.target.tagName === "img") {
+    event.preventDefault();
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll("img");
   images.forEach(function (image) {
@@ -7,3 +24,27 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(searchBtn) && !e.composedPath().includes(searchForm)
+    ) {
+      searchForm.classList.remove("active");
+    }
+  })
+});
+
+cartBtn.addEventListener("click", function () {
+  cartItem.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(cartBtn) &&
+      !e.composedPath().includes(cartItem)
+    ) {
+      cartItem.classList.remove("active");
+    }
+  });
+});
