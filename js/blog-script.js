18676b86a0e7daf7d93e2dc02d4cd4a4a9961dda
Berkan K <berkan0hk@gmail.com>
2024-10-02 20:10:41 +0300

    site done for real this time literally

---
+++ js/blog-script.js
@@ -0,0 +1,20 @@
+const posts = document.querySelectorAll('.blog-post');
+
+posts.forEach(post => {
+  post.addEventListener('click', function () {
+    const isActive = this.classList.contains('active');
+
+    if (!isActive) {
+      posts.forEach(p => p.classList.remove('active'));
+      this.classList.add('active');
+      document.body.classList.add('active');
+    }
+
+    document.addEventListener('click', (e) => {
+      if (!e.composedPath().includes(post)) {
+        post.classList.remove('active');
+        document.body.classList.remove('active'); // to open the damn scroll
+      }
+    });
+  });
+});
